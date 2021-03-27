9926e4e41333d19b5a8119c84461e0aab292bbef
Omar Elsobkey <omarelsobkey9009@gmail.com>
2021-03-27 22:01:27 +0200

    Add comments to make the formula more clear.

--- Robot Car/Robot Car with Ultrasonic/ultrasonic/ultrasonic.ino
+++ Robot Car/Robot Car with Ultrasonic/ultrasonic/ultrasonic.ino
@@ -1,12 +1,12 @@
 /********************************************************************
-Engineer : Omar Elsobkey
-Version  : 1.0
-Date     : 9/1/2021
+  Engineer : Omar Elsobkey
+  Version  : 1.0
+  Date     : 9/1/2021
 ********************************************************************/
 
 #define trigger 10
 #define echo 11
-int distancem duration;
+int distance, duration;
 void setup() {
   // put your setup code here, to run once:
   pinMode(trigger, OUTPUT);
@@ -20,7 +20,8 @@ void loop() {
   digitalWrite(trigger, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigger, LOW);
-  duration = pulseIn(echo, HIGH);
-  distance = duration * 0.034 / 2;
+  duration = pulseIn(echo, HIGH);  // returns duration in microseconds
+  distance = duration * 0.034 / 2; // ( duration * 10^-3 ) * speed of waves (340m/s) / 2
+                                   // 2 is (half of the time that wave traveled from trigger to obstacle then again to echo)
   Serial.println(distance);
 }
